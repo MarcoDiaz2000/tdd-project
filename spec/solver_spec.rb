5341ff85087ca11e24bd955cffea32c9ac0450aa
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-03 02:34:01 -0600

    Add test for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe Solver do
       expect(solver.reverse('marco 123')).to eq('321 ocram')
     end
   end
+
+  describe "fizzbuzz" do
+
+    it "return 'fizz' when given a number divisible by 3" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it "return 'buzz' when given a number dividible by 5" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it "return 'fizzbuzz' when given a number dividible by 3" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it "return number as string when given a number non-divisible by 3 or 5" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end

23d23aeeab337f57aa6ccede1fbb0ec844e76e39
danielsafari143 <danielsafari143@gmail.com>
2023-08-03 11:06:11 +0200

    FIRST FACTORIAL TEST

--- solver.rb
+++ solver.rb
@@ -4,6 +4,16 @@ class Solver
   end
 
   def factorial(realNumber)
-    
+    def calculate(realNumber , factorialResult , constantOutPut)
+      factorialNumber = realNumber - 1
+      factorialResult *= factorialNumber
+      if factorialNumber == 1
+        return factorialResult * constantOutPut
+      end
+  
+      calculate(factorialNumber , factorialResult , constantOutPut)
+    end
+
+    return calculate(realNumber , 1 , realNumber)
   end
 end
